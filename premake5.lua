6f5b6545cc15fa6dca503133c08ee7c20f90debd
islander <miku.miku.miku@sjtu.edu.cn>
2022-08-22 16:09:49 +0800

    fix(premake5): fix premake file

--- premake5.lua
+++ premake5.lua
@@ -2,11 +2,11 @@ project "GLFW"
     kind "StaticLib"
     language "C"
     
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
-	{
+    files
+    {
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
         "src/glfw_config.h",
@@ -18,10 +18,10 @@ project "GLFW"
         "src/window.c"
     }
     
-	filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
-        systemversion "10.0.17134.0"
-        staticruntime "On"
+    filter "system:windows"
+        cppdialect "c++17"
+        staticruntime "on"
+        systemversion "latest"
         
         files
         {
@@ -36,10 +36,10 @@ project "GLFW"
             "src/osmesa_context.c"
         }
 
-		defines 
-		{ 
+        defines 
+        { 
             "_GLFW_WIN32",
             "_CRT_SECURE_NO_WARNINGS"
-		}
+        }
     filter { "system:windows", "configurations:Release" }
         buildoptions "/MT"
